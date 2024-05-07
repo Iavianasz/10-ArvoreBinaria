8c680043d00b2cdeb2d919c16fad6552d8de56bc
Iago Viana <iago63803@gmail.com>
2024-05-07 11:59:57 -0300

    adicionando a funcionalidade de exibir elementos

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -152,5 +152,11 @@ int elementosArvore(NO* no)
 
 void exibirElementosArvore(NO* no)
 {
-	
+	if (no == NULL)
+		return;
+
+	cout << no->valor << endl;
+
+	exibirElementosArvore(no->dir);
+	exibirElementosArvore(no->esq);
 }
